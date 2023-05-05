63b1612a2b7ce922105162bc747c64fdbb3213a7
RESILIENTOMAR <TEIXEIRAOMAR1@GMAIL.COM>
2023-05-05 16:18:23 -0400

    bug fix

--- ssb-personal-express/server.js
+++ ssb-personal-express/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = 8005
+app.set('view engine', 'ejs');
 
-app.use(cors())
+
+//app.use(cors())
 
 /*=======================OUTFITS===========================*/
 
@@ -15,7 +17,7 @@ const character = {
         'origin': 'Donkey Kong',
         'homeWorld': 'Mushroom Kingdom',
         'pic': 'marioOutfit1.jpeg',
-        'outfit3':'marioOutfit2.jpeg',
+        'outfit3': 'marioOutfit2.jpeg',
         'outfit2': 'marioOutfit3.jpeg'
     },
     'luigi': {
@@ -23,96 +25,96 @@ const character = {
         'origin': 'Mario Bros.',
         'homeWorld': 'Mushroom Kingdom',
         'pic': 'luigi.jpeg',
-        'outfit3':'luigi-mansion.jpeg',
-        'outfit2':'baby-luigi.jpeg'
+        'outfit3': 'luigi-mansion.jpeg',
+        'outfit2': 'baby-luigi.jpeg'
     },
     'donkey kong': {
         'name': 'Donkey Kong',
         'origin': 'Donkey Kong Arcade Game ',
         'homeWorld': 'Donkey Kong Island',
         'pic': 'donkeykong.jpeg',
-        'outfit3':'pinkDK.jpeg',
-        'outfit2':'grayDK.jpeg'
+        'outfit3': 'pinkDK.jpeg',
+        'outfit2': 'grayDK.jpeg'
     },
     'link': {
         'name': 'Link',
         'origin': 'The Legend of Zelda',
         'homeWorld': 'Hyrule',
         'pic': 'link1.jpeg',
-        'outfit2':'toonLink.jpeg',
-        'outfit3':'link2.jpeg'
+        'outfit2': 'toonLink.jpeg',
+        'outfit3': 'link2.jpeg'
     },
     'samus': {
         'name': 'Samus',
         'origin': 'Metroid',
         'homeWorld': 'Earth colony K-2L',
         'pic': 'samus.jpeg',
-        'outfit3':'zeroSuit.jpeg',
-        'outfit2':'lightSamus.jpeg'
+        'outfit3': 'zeroSuit.jpeg',
+        'outfit2': 'lightSamus.jpeg'
     },
     'captain falcon': {
         'name': 'Captain Falcon',
         'origin': 'F-Zero',
         'homeWorld': 'Port Town',
         'pic': 'captainfalcon.jpeg',
-        'outfit3':'pinkFalcon.jpeg',
-        'outfit2':'goldFalcon.jpeg'
+        'outfit3': 'pinkFalcon.jpeg',
+        'outfit2': 'goldFalcon.jpeg'
     },
     'yoshi': {
         'name': 'Yoshi',
         'origin': 'Super Mario World',
         'homeWorld': "Yoshi's Island",
         'pic': 'yoshi.jpeg',
-        'outfit3':'Black_Yoshi.jpeg',
-        'outfit2':'Red_Yoshi.jpeg'
-        
+        'outfit3': 'Black_Yoshi.jpeg',
+        'outfit2': 'Red_Yoshi.jpeg'
+
     },
     'ness': {
         'name': 'Ness',
         'origin': 'EarthBound',
         'homeWorld': 'Onett in Eagleland',
         'pic': 'ness.jpeg',
-        'outfit3':'Ness_Palette_06.jpeg',
-        'outfit2':'Ness_Palette_08.jpeg'
+        'outfit3': 'Ness_Palette_06.jpeg',
+        'outfit2': 'Ness_Palette_08.jpeg'
     },
     'kirby': {
         'name': 'Kirby',
         'origin': "Kirby's Dream Land",
         'homeWorld': 'Dream Land',
         'pic': 'kirby.jpeg',
-        'outfit3':'kirby-fir.jpeg',
-        'outfit2':'icekirby.jpeg'
+        'outfit3': 'kirby-fir.jpeg',
+        'outfit2': 'icekirby.jpeg'
     },
     'fox': {
         'name': 'Fox',
         'origin': 'Star Fox 64 ',
         'homeWorld': 'Final Destination',
         'pic': 'fox.jpeg',
-        'outfit3':'pinkFox.jpeg',
-        'outfit2':'purpleFox.jpeg'
+        'outfit3': 'pinkFox.jpeg',
+        'outfit2': 'purpleFox.jpeg'
     },
     'pikachu': {
         'name': 'Pikachu',
         'origin': 'Pokémon Red and Green',
         'homeWorld': 'Pokemon World',
         'pic': 'pika1.jpeg',
-        'outfit3':'pikaLibre.jpeg',
-        'outfit2':'detectivePika.jpeg'
+        'outfit3': 'pikaLibre.jpeg',
+        'outfit2': 'detectivePika.jpeg'
     },
     'jigglypuff': {
         'name': 'Jigglypuff',
         'origin': 'Pokémon Red and Blue',
         'homeWorld': 'Kanto ',
         'pic': 'jiggly.jpeg',
-        'outfit3':'jigglyFlower.jpeg',
-        'outfit2':'jigglyHat.jpeg'
+        'outfit3': 'jigglyFlower.jpeg',
+        'outfit2': 'jigglyHat.jpeg'
     },
-    'leon':{
-        'name':'Leon Noel',
+    'leon': {
+        'name': 'Leon Noel',
         'origin': 'South Philly',
-        'homeWorld':'Earth',
+        'homeWorld': 'Earth',
         'pic': 'leon.jpeg',
-        'outfit2':'leonflex.jpeg'
+        'outfit2': 'leonflex.jpeg'
     },
 
     'unknown': {
@@ -123,7 +125,7 @@ const character = {
     }
 }
 app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.ejs')
+    response.render(__dirname + '/index.ejs')
 })
 
 app.get('/js/main.js', (request, response) => {
@@ -158,8 +160,8 @@ app.put('/api/:characterName', (req, res) => {
     const newCharacterData = req.body;
 
     res.json({ message: `Updated character data for ${characterName}` });
-  });
-  
+});
+
 
 
 app.listen(process.env.PORT || PORT, () => {
